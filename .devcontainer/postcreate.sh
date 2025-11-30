5e3b2b6f166df45da5fafdbd7f03379c89640478
Hendrik Bruinsma <hbruinsma@xs4some.nl>
2025-11-30 13:53:40 +0000

    Add GitHub cli to the devcontainer

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -85,6 +85,17 @@ if [ ! -d "$HOME/.oh-my-bash" ]; then
     git clone --depth=1 https://github.com/ohmybash/oh-my-bash.git "$HOME/.oh-my-bash"
 fi
 
+# Install GitHub CLI
+
+echo "Installing GitHub CLI..."
+if ! command -v gh &> /dev/null; then
+    curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+        && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+        && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+        && sudo apt-get update \
+        && sudo apt-get install -y gh
+fi
+
 # Install Podman if not already installed
 echo "Installing Podman..."
 if ! command -v podman &> /dev/null; then
